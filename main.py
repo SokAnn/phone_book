182ff3f399a32320d37075a36bd01fc02c327e05
Ann Sokolova <fullsokan@gmail.com>
2021-09-21 15:46:56 +0300

    changing version

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-# Python version 2.7
-
+# Python version 3.8
 
 def print_hi(name):
-    print 'Hi, ' + name
+    print('Hi, ' + name)
 
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print 'test'
+    print('test')
